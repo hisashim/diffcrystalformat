0059fe9c91634a5c20640a90c3ea5ac1a96740ff
Hisashi Morita <hisashim@workbook.org>
2021-12-01 12:56:05 +0900

    Add minimum spec

--- spec/diff_crystal_format_spec.cr
+++ spec/diff_crystal_format_spec.cr
@@ -4,4 +4,22 @@ describe DiffCrystalFormat do
   it "has version number" do
     DiffCrystalFormat::VERSION.should match(/\A\d+(?:\.\d+)*\z/)
   end
+
+  describe ".diffs" do
+    it "returns diffs between before and after formatting" do
+      src = <<-EOS + "\n"
+        p  1
+        EOS
+      expected = <<-EOS + "\n"
+        -p  1
+        +p 1
+        EOS
+      tf = File.tempfile { |f| File.write(f.path, src) }
+
+      actual = DiffCrystalFormat.diffs([tf.path], nil).map { |diff|
+        diff.lines(chomp: false)[3..].join # Skip diff header (first 3 lines)
+      }
+      actual.should eq [expected]
+    end
+  end
 end
